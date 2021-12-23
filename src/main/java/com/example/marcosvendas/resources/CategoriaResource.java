0d5490173d7a27f59ea8255f7b7b52fff721d907
Marcosjedai <alan325_batera@hotmail.com>
2021-12-23 19:13:51 -0300

    Criando Categoria service e repository

--- src/main/java/com/example/marcosvendas/resources/CategoriaResource.java
+++ src/main/java/com/example/marcosvendas/resources/CategoriaResource.java
@@ -1,19 +1,27 @@
 package com.example.marcosvendas.resources;
 
 import com.example.marcosvendas.domain.Categoria;
+import com.example.marcosvendas.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
+ @Autowired
+ CategoriaService categoriaService;
+
  @RequestMapping(method = RequestMethod.GET)
-    public  List<Categoria> find(){
+    public  List<Categoria> findAll(){
 
      Categoria cat = new Categoria(1, "Informatica");
      Categoria cat2 = new Categoria(2, "Escritório");
@@ -23,5 +31,12 @@ public class CategoriaResource {
       categoriaList.add(cat2);
       return categoriaList;
  }
+
+ @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+ public ResponseEntity<?> findById(@PathVariable Integer id){
+  Categoria categoriaOptional = categoriaService.finById(id);
+   return ResponseEntity.ok().body(categoriaOptional);
+ }
+
 
 }
