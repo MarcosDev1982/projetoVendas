bf2dd113536bce2d6e89d4d28afa66285fdd36bc
Marcosjedai <alan325_batera@hotmail.com>
2021-12-23 16:11:57 -0300

    Classe dominio Criado

--- src/main/java/com/example/marcosvendas/resources/CategoriaResource.java
+++ src/main/java/com/example/marcosvendas/resources/CategoriaResource.java
@@ -1,10 +1,27 @@
 package com.example.marcosvendas.resources;
 
+import com.example.marcosvendas.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping("/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+ @RequestMapping(method = RequestMethod.GET)
+    public  List<Categoria> find(){
+
+     Categoria cat = new Categoria(1, "Informatica");
+     Categoria cat2 = new Categoria(2, "Escritório");
+
+      List<Categoria> categoriaList = new ArrayList<>();
+      categoriaList.add(cat);
+      categoriaList.add(cat2);
+      return categoriaList;
+ }
 
 }
