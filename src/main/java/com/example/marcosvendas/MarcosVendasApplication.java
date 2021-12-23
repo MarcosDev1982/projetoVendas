5252fe51c9eb6755203ced4426abe31abe455cbc
Marcosjedai <alan325_batera@hotmail.com>
2021-12-23 13:38:44 -0300

    Criação do projeto

---
+++ src/main/java/com/example/marcosvendas/MarcosVendasApplication.java
@@ -0,0 +1,13 @@
+package com.example.marcosvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcosVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcosVendasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marcosvendas/MarcosVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marcosvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcosVendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
