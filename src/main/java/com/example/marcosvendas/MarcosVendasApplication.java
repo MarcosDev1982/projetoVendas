3ffec2077d0d13464f08e5052e0221b96bf71751
Marcosjedai <alan325_batera@hotmail.com>
2021-12-24 09:22:47 -0300

    Estado e cidade

--- src/main/java/com/example/marcosvendas/MarcosVendasApplication.java
+++ src/main/java/com/example/marcosvendas/MarcosVendasApplication.java
@@ -1,8 +1,12 @@
 package com.example.marcosvendas;
 
 import com.example.marcosvendas.domain.Categoria;
+import com.example.marcosvendas.domain.Cidade;
+import com.example.marcosvendas.domain.Estado;
 import com.example.marcosvendas.domain.Produto;
 import com.example.marcosvendas.repository.CategoriaRepositories;
+import com.example.marcosvendas.repository.CidadeRepositories;
+import com.example.marcosvendas.repository.EstadoRepositories;
 import com.example.marcosvendas.repository.ProdutoRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,6 +23,12 @@ public class MarcosVendasApplication implements CommandLineRunner {
 
     @Autowired
     ProdutoRepositories produtoRepositories;
+
+    @Autowired
+    CidadeRepositories cidadeRepositories;
+
+    @Autowired
+    EstadoRepositories estadoRepositories;
 
     public static void main(String[] args) {
         SpringApplication.run(MarcosVendasApplication.class, args);
@@ -41,8 +51,24 @@ public class MarcosVendasApplication implements CommandLineRunner {
         p3.getCategorias().addAll(Arrays.asList(cat1));
 
 
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlandia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+
+
         categoriaRepositories.saveAll(Arrays.asList(cat1, cat2));
 
         produtoRepositories.saveAll(Arrays.asList(p1, p2, p3));
+
+        estadoRepositories.saveAll(Arrays.asList(est1, est2));
+
+        cidadeRepositories.saveAll(Arrays.asList(c1,c2,c3));
     }
 }
